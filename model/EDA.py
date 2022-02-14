cd6b3c9030da9b0a2644ffb93e0ae233a85a4641
Huy Huynh <49454101+huytjuh@users.noreply.github.com>
2022-02-14 20:24:26 +0100

    Update EDA.py

--- model/EDA.py
+++ model/EDA.py
@@ -4,3 +4,17 @@ import datetime
 
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+
+class EDA:
+  
+    def __init__(self, data):
+        self.data = data
+
+    def pair_plot(self, bool_cat=True):
+        
+        data = self.data
+        g = sns.pairplot(data=data, vars=data.columns)
+        
+        return g
+
