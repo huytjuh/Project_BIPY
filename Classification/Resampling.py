300c5865ed8f6eb82509457c317325733d76e6af
Huy Huynh <49454101+huytjuh@users.noreply.github.com>
2022-03-07 23:36:00 +0100

    Update Resampling.py

--- Classification/Resampling.py
+++ Classification/Resampling.py
@@ -1,6 +1,7 @@
 ### CLASS IMBALANCE ###
 
 from imblearn.over_sampling import RandomOverSampler, SMOTE, SMOTENC, BorderlineSMOTE, SVMSMOTE, ADASYN
+from imblearn.under_sampling import TomekLinks 
 from sklearn.utils import resample
 
 def naive_resample(df, target, random_seed=1234):
@@ -11,12 +12,16 @@ def naive_resample(df, target, random_seed=1234):
 def SNOTE_resample(df, target, method='naive', random_seed=1234):
     list_SNOTE = {'naive': SMOTE(random_state=random_seed), 'NC': SMOTENC([df.columns.get_loc(col) for col in df.select_dtypes(include=['object']).columns], random_state=random_seed),
                   'borderline': BorderlineSMOTE(random_state=random_seed, kind='borderline-1'), 'SVM': SVMSMOTE(random_state=random_seed)}
-    print(list_SNOTE[method])
     X, y = list_SNOTE[method].fit_resample(df.drop(columns=target), df[[target]])
     df_output = pd.concat([X, y], axis=1)
     return df_output
 
 def ADASYN_resample(df, target, random_seed=1234):
     X, y = ADASYN(random_state=random_seed).fit_resample(df.drop(columns=target), df[[target]])
+    df_output = pd.concat([X, y], axis=1)
+    return df_output
+
+def tomeklinks_resample(df, target, random_seed=1234):
+    X, y = TomekLinks(random_state=random_seed).fit_resample(df.drop(columns=target), df[[target]])
     df_output = pd.concat([X, y], axis=1)
     return df_output
