db60e122c265a076df2c7a67518319a0c019f82c
Huy Huynh <49454101+huytjuh@users.noreply.github.com>
2022-03-09 17:01:39 +0100

    Update Outlier_Detection.py

--- Classification/Outlier_Detection.py
+++ Classification/Outlier_Detection.py
@@ -1,9 +1,20 @@
 ### OUTLIER DETECTION ###
 
-def naive_outlier():
+from sklearn.svm import OneClassSVM
+from sklearn.ensemble import IsolationForest
 
+def OneClassSVM_outlier(df, contamination=0.1, dict_param={}, random_seed=1234):    
+    OCSVM = OneClassSVM(nu=contamination, *dict_param).fit(df)
+    df_output = df.copy()
+    df_output['Outlier'] = [1 if x == -1 else 0 for x in OCSVM.predict(df)]
+    return df_output
+
+def IsolationForest_outlier(df, n_estimators=1000, contamination=0.1, dict_param={}, random_seed=1234):
+    IF = IsolationForest(contamination=contamination, *dict_param).fit(df)
+    df_output = df.copy()
+    df_output['Outlier'] = [1 if x == -1 else 0 for x in IF.predict(df)]
     return
 
-def IsolationForest_outlier():
-
-    return
+def FSRMCD_MAC_outlier(df):
+    output = get_fsrmcdmac_outliers(df)
+    return output
