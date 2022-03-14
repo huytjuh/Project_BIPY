f75e9f4177cbaf64cfdd51f003f7039c27fc3d94
Huy Huynh <49454101+huytjuh@users.noreply.github.com>
2022-03-14 13:42:11 +0100

    Create Evaluation_RS.py

---
+++ Recommender_Systems/Evaluation_RS.py
@@ -0,0 +1,20 @@
+### EVALUATION ###
+
+def mAP():
+    """ MEAN AVERAGE PRECISION @p
+    
+    """
+    return
+
+def DCG():
+    """ DISCOUNTED CUMULATIVE GAIN
+
+    """
+    return
+
+def personalization_index():
+    """ PERSONALIZATION INDEX
+    Measures how unique recommendations are to a user. It computes the distance between each pair of recommendation
+    and then the average of that. To compare different personalization indices, we normalize them.
+    """
+    return
